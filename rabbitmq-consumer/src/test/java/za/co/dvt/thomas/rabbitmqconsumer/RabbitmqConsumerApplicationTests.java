ae9e98fc206b0f3f1cbe0ad26760577db3b9e26c
Thomas McAlpine <tmcalpine@jhb.dvt.co.za>
2022-06-17 08:35:14 +0200

    Added rabbitmq login

---
+++ rabbitmq-consumer/src/test/java/za/co/dvt/thomas/rabbitmqconsumer/RabbitmqConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package za.co.dvt.thomas.rabbitmqconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitmqConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ rabbitmq-producer/src/test/java/za/co/dvt/thomas/rabbitmqproducer/RabbitmqProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package za.co.dvt.thomas.rabbitmqproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitmqProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
